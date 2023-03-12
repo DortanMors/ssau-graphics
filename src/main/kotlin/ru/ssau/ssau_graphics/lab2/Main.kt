a9c86e0e31450cdd6aa7e376c1bcddd667ab0f43
Fomin <vyacheslav.fomin@magenta-technology.com>
2023-03-13 01:07:24 +0400

    lab2 task 14

--- src/main/kotlin/ru/ssau/ssau_graphics/lab2/Main.kt
+++ src/main/kotlin/ru/ssau/ssau_graphics/lab2/Main.kt
@@ -1,14 +1,14 @@
 package ru.ssau.ssau_graphics.lab2
 
 import ru.ssau.ssau_graphics.lab1.io.readPolygonalModelFromFile
-import ru.ssau.ssau_graphics.lab1.model.Color3
-import ru.ssau.ssau_graphics.lab1.model.Coordinate
-import ru.ssau.ssau_graphics.lab1.model.Point2d
-import ru.ssau.ssau_graphics.lab1.model.Polygon
+import ru.ssau.ssau_graphics.lab1.model.*
 import ru.ssau.ssau_graphics.lab2.barycentric.toBarycentric
 import ru.ssau.ssau_graphics.lab2.draw.drawPolygonTriangle
+import ru.ssau.ssau_graphics.lab2.math.findNormal
+import ru.ssau.ssau_graphics.lab2.math.normalizedScalarMult
 import ru.ssau.ssau_graphics.utils.createImage3
 import ru.ssau.ssau_graphics.utils.saveImage
+import kotlin.math.abs
 import kotlin.random.Random
 
 private fun task8n1() {
@@ -51,9 +51,8 @@ private fun task10n1(h: Int = 100, w: Int = 100) {
     }
 }
 
-fun task11n1(filename: String, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
+fun task11n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
     val image = createImage3(1000, 1000)
-    val model = readPolygonalModelFromFile(filename)
     model.polygons
         .map { p ->
             Polygon(
@@ -71,8 +70,61 @@ fun task11n1(filename: String, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, 
     saveImage(image, "task11n1")
 }
 
+fun task13n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
+    val image = createImage3(1000, 1000)
+    val lightDirection = Coordinate(0.0, 0.0, 1.0)
+    model.polygons
+        .filter { polygon ->
+            normalizedScalarMult(findNormal(polygon), lightDirection) < 0
+        }.map { p ->
+            Polygon(
+                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, 0.0),
+                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, 0.0),
+                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, 0.0),
+            )
+        }.forEach { polygon ->
+            drawPolygonTriangle(
+                image,
+                polygon,
+                Color3(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
+            )
+        }
+    saveImage(image, "task13n1")
+}
+
+fun task14n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
+    val image = createImage3(1000, 1000)
+    val lightDirection = Coordinate(1.0, 0.0, 0.4)
+    model.polygons
+        .filter { polygon ->
+            normalizedScalarMult(findNormal(polygon), lightDirection) < 0
+        }.map { p ->
+            Polygon(
+                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, 0.0),
+                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, 0.0),
+                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, 0.0),
+            )
+        }.forEach { polygon ->
+            val shadow = abs(normalizedScalarMult(findNormal(polygon), lightDirection))
+            drawPolygonTriangle(
+                image,
+                polygon,
+                Color3(
+                    0,
+                    (shadow * 255).toInt(),
+                    0,
+                ),
+            )
+        }
+    saveImage(image, "task14n1")
+}
+
 fun main() {
-    task8n1()
-    task10n1()
-    task11n1("model_1.obj")
+    val modelName = "model_1.obj"
+//    task8n1()
+//    task10n1()
+    val model = readPolygonalModelFromFile(modelName)
+//    task11n1(model)
+//    task13n1(model)
+    task14n1(model)
 }
---
+++ src/main/kotlin/ru/ssau/ssau_graphics/lab2/math/Algebra.kt
@@ -0,0 +1,34 @@
+package ru.ssau.ssau_graphics.lab2.math
+
+import org.jetbrains.kotlinx.multik.api.linalg.dot
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarray
+import ru.ssau.ssau_graphics.lab1.model.Coordinate
+import ru.ssau.ssau_graphics.lab1.model.Polygon
+import kotlin.math.sqrt
+
+fun findNormal(polygon: Polygon): Coordinate {
+    val a = Coordinate(
+        polygon.v2.x - polygon.v1.x,
+        polygon.v2.y - polygon.v1.y,
+        polygon.v2.z - polygon.v1.z,
+    )
+    val b = Coordinate(
+        polygon.v2.x - polygon.v3.x,
+        polygon.v2.y - polygon.v3.y,
+        polygon.v2.z - polygon.v3.z,
+    )
+    return vectorMult(a, b)
+}
+
+fun vectorMult(a: Coordinate, b: Coordinate): Coordinate =
+    Coordinate(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+
+val Coordinate.length
+    get() = sqrt(x * x + y * y + z * z)
+
+fun normalizedScalarMult(left: Coordinate, right: Coordinate): Double {
+    val a = mk.ndarray(mk[left.x, left.y, left.z])
+    val b = mk.ndarray(mk[right.x, right.y, right.z])
+    return a.dot(b) / (left.length * right.length)
+}
