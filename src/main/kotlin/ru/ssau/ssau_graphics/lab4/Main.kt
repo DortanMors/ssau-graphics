d2ef2452f0af0e82f8f91379f17aa47649c87cbc
Fomin <vyacheslav.fomin@magenta-technology.com>
2023-04-20 17:37:53 +0400

    lab4 масштабирование

--- src/main/kotlin/ru/ssau/ssau_graphics/lab4/Main.kt
+++ src/main/kotlin/ru/ssau/ssau_graphics/lab4/Main.kt
@@ -1,7 +1,9 @@
 package ru.ssau.ssau_graphics.lab4
 
+import ru.ssau.ssau_graphics.lab1.io.readPolygonalModelFromFile
 import ru.ssau.ssau_graphics.lab1.model.*
 import ru.ssau.ssau_graphics.lab2.draw.drawPolygonTriangleWithZ
+import ru.ssau.ssau_graphics.lab2.draw.prepare
 import ru.ssau.ssau_graphics.lab2.math.findNormal
 import ru.ssau.ssau_graphics.lab2.math.normalizedScalarMult
 import ru.ssau.ssau_graphics.utils.createImage3
@@ -9,23 +11,21 @@ import ru.ssau.ssau_graphics.utils.saveImage
 import kotlin.math.abs
 
 fun main() {
-
+    val modelName = "model_1.obj"
+    val h = 1000
+    val w = 1000
+    val model = readPolygonalModelFromFile(modelName)
+        .prepare(h, w, 10000.0, 10000.0, listOf(0.005, -0.045, 15.0)) // масштабирование
+    task15n2(model, h, w)
 }
 
-fun task15n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
+fun task15n2(model: PolygonalModel, h: Int, w: Int) {
     val image = createImage3(h, w)
     val zMatrix = Image(h, w, Array(h * w) { 10000.0 })
     val lightDirection = Coordinate(1.0, 0.0, 0.4)
     model.polygons
-        .filter { polygon ->
-            normalizedScalarMult(findNormal(polygon), lightDirection) < 0 // отсеивание задней стороны
-        }.map { p ->
-            Polygon(
-                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, p.v1.z),
-                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, p.v2.z),
-                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, p.v3.z),
-            ) // масштабирование
-        }.forEach { polygon ->
+        .filter { polygon -> normalizedScalarMult(findNormal(polygon), lightDirection) < 0 } // отсеивание задней стороны
+        .forEach { polygon ->
             val shadow = abs(normalizedScalarMult(findNormal(polygon), lightDirection))
             drawPolygonTriangleWithZ(
                 image,
@@ -38,5 +38,5 @@ fun task15n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5
                 ),
             ) // отрисовка полигона с z-буффером
         }
-    saveImage(image, "task15n1")
+    saveImage(image, "task15n2")
 }
