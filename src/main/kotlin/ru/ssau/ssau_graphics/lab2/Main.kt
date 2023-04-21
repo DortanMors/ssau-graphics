1d5cc24baf3337fa1cfd385b7c4ba5bf5d94aa9e
Fomin <vyacheslav.fomin@magenta-technology.com>
2023-04-21 23:46:21 +0400

    Реструктурирован вызывающий код (main)

--- src/main/kotlin/ru/ssau/ssau_graphics/lab2/Main.kt
+++ src/main/kotlin/ru/ssau/ssau_graphics/lab2/Main.kt
@@ -4,26 +4,20 @@ import ru.ssau.ssau_graphics.common.io.readPolygonalModelFromFile
 import ru.ssau.ssau_graphics.common.model.*
 import ru.ssau.ssau_graphics.common.barycentric.toBarycentric
 import ru.ssau.ssau_graphics.common.draw.drawPolygonTriangle
-import ru.ssau.ssau_graphics.common.draw.drawPolygonTriangleWithZ
-import ru.ssau.ssau_graphics.common.math.findNormal
-import ru.ssau.ssau_graphics.common.math.normalizedScalarMult
 import ru.ssau.ssau_graphics.common.utils.createImage3
 import ru.ssau.ssau_graphics.common.utils.saveImage
-import kotlin.math.abs
 import kotlin.random.Random
 
 fun main() {
     val modelName = "model_1.obj"
+    task7n1()
     task8n1()
-    task10n1()
     val model = readPolygonalModelFromFile(modelName)
-    task11n1(model)
-    task13n1(model)
-    task14n1(model)
-    task15n1(model)
+    task10n1(model)
 }
 
-private fun task8n1() {
+// Барицентрические координаты
+private fun task7n1() {
     val point = Point2d(1, 5)
     val polygon = Polygon(
         v1 = Coordinate(0.0, 0.0, 0.0),
@@ -31,11 +25,12 @@ private fun task8n1() {
         v3 = Coordinate(0.0, 10.0, 1.0),
     )
     toBarycentric(polygon, point).run {
-        println("($x) + ($y) + ($z) = ${x + y + z}")
+        println("task7: Сумма барицентрических координат = ($x) + ($y) + ($z) = ${x + y + z}")
     }
 }
 
-private fun task10n1(h: Int = 100, w: Int = 100) {
+// Отрисовка треугольников
+private fun task8n1(h: Int = 100, w: Int = 100) {
     listOf(
         Polygon(
             v1 = Coordinate(0.0, 0.0, 0.0),
@@ -59,11 +54,12 @@ private fun task10n1(h: Int = 100, w: Int = 100) {
             polygon,
             Color3(255, 255, 255),
         )
-        saveImage(image, "task10n$i")
+        saveImage(image, "task8n$i")
     }
 }
 
-fun task11n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
+// Отрисовка полигонов трёхмерной модели
+fun task10n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
     val image = createImage3(h, w)
     model.polygons
         .map { p ->
@@ -79,83 +75,5 @@ fun task11n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5
             Color3(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
         )
     }
-    saveImage(image, "task11n1")
-}
-
-fun task13n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
-    val image = createImage3(h, w)
-    val lightDirection = Coordinate(0.0, 0.0, 1.0)
-    model.polygons
-        .filter { polygon ->
-            normalizedScalarMult(findNormal(polygon), lightDirection) < 0
-        }.map { p ->
-            Polygon(
-                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, 0.0),
-                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, 0.0),
-                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, 0.0),
-            )
-        }.forEach { polygon ->
-            drawPolygonTriangle(
-                image,
-                polygon,
-                Color3(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)),
-            )
-        }
-    saveImage(image, "task13n1")
-}
-
-fun task14n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
-    val image = createImage3(h, w)
-    val lightDirection = Coordinate(1.0, 0.0, 0.4)
-    model.polygons
-        .filter { polygon ->
-            normalizedScalarMult(findNormal(polygon), lightDirection) < 0
-        }.map { p ->
-            Polygon(
-                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, 0.0),
-                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, 0.0),
-                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, 0.0),
-            )
-        }.forEach { polygon ->
-            val shadow = abs(normalizedScalarMult(findNormal(polygon), lightDirection))
-            drawPolygonTriangle(
-                image,
-                polygon,
-                Color3(
-                    0,
-                    (shadow * 255).toInt(),
-                    0,
-                ),
-            )
-        }
-    saveImage(image, "task14n1")
-}
-
-fun task15n1(model: PolygonalModel, h: Int = 1000, w: Int = 1000, xMult: Int = 5000, xDelta: Int = 500, yMult: Int = 5000, yDelta: Int = 500) {
-    val image = createImage3(h, w)
-    val zMatrix = Image(h, w, Array(h * w) { 10000.0 })
-    val lightDirection = Coordinate(1.0, 0.0, 0.4)
-    model.polygons
-        .filter { polygon ->
-            normalizedScalarMult(findNormal(polygon), lightDirection) < 0 // отсеивание задней стороны
-        }.map { p ->
-            Polygon(
-                Coordinate(xMult * p.v1.x + xDelta, yMult * p.v1.y + yDelta, p.v1.z),
-                Coordinate(xMult * p.v2.x + xDelta, yMult * p.v2.y + yDelta, p.v2.z),
-                Coordinate(xMult * p.v3.x + xDelta, yMult * p.v3.y + yDelta, p.v3.z),
-            ) // масштабирование
-        }.forEach { polygon ->
-            val shadow = abs(normalizedScalarMult(findNormal(polygon), lightDirection))
-            drawPolygonTriangleWithZ(
-                image,
-                zMatrix,
-                polygon,
-                Color3(
-                    0,
-                    (shadow * 255).toInt(),
-                    0,
-                ),
-            ) // отрисовка полигона с z-буффером
-        }
-    saveImage(image, "task15n1")
+    saveImage(image, "task10n1")
 }
--- src/main/kotlin/ru/ssau/ssau_graphics/lab4/Main.kt
+++ src/main/kotlin/ru/ssau/ssau_graphics/lab4/Main.kt
@@ -16,16 +16,33 @@ fun main() {
     val h = 2500
     val w = 2500
     val model = readPolygonalModelFromFile(modelName)
-        .pivot(0.0, 180.0, 0.0)
-        .prepare(100000.0, 100000.0, 400.0, 400.0, listOf(0.005, -0.045, 15.0)) // масштабирование
-    task16n1(model, h, w)
+    task16n1(
+        model,
+        h, w, // Размер изображения
+        0.0, 0.0, 0.0, // Углы поворота
+        100000.0, 100000.0,  // Масштабирование
+        400.0, 400.0,        // Смещение изображения
+        listOf(0.005, -0.045, 15.0), // Смешение модели
+        Coordinate(1.0, 0.0, 1.0), // Направление света
+    )
 }
 
-fun task16n1(model: PolygonalModel, h: Int, w: Int) {
+// Рендер повёрнутой и масштабированной модели с z-буффером и простым освещением
+fun task16n1(
+    model: PolygonalModel,
+    h: Int, w: Int, // Размер изображения
+    alphaDegree: Double, betaDegree: Double, gammaDegree: Double, // Углы поворота
+    ax: Double, ay: Double, // Масштабирование
+    u0: Double, v0: Double, // Смещение изображения
+    tList: List<Double>,    // Смешение модели
+    lightDirection: Coordinate, // Направление света
+) {
+    val preparedModel = model
+        .pivot(alphaDegree, betaDegree, gammaDegree) // поворот модели
+        .prepare(ax, ay, u0, v0, tList)              // масштабирование
     val image = createImage3(h, w)
     val zMatrix = Image(h, w, Array(h * w) { Double.POSITIVE_INFINITY })
-    val lightDirection = Coordinate(1.0, 0.0, 1.0)
-    model.polygons
+    preparedModel.polygons
         .filter { polygon -> normalizedScalarMult(findNormal(polygon), lightDirection) > 0 } // отсеивание задней стороны
         .forEach { polygon ->
             val shadow = abs(normalizedScalarMult(findNormal(polygon), lightDirection))
